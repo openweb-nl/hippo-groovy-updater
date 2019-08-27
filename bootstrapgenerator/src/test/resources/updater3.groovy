cbea37345ab276f54f52d085d69b27ba2381fc3c
Joost Oudeman <joost@openweb.nl>
2019-08-27 10:31:12 +0200

    Extend test to expose problem

--- bootstrapgenerator/src/test/resources/updater3.groovy
+++ bootstrapgenerator/src/test/resources/updater3.groovy
@@ -6,7 +6,7 @@ import javax.jcr.Node
 import nl.openweb.hippo.groovy.annotations.Bootstrap.ContentRoot;
 @Bootstrap(contentroot = ContentRoot.QUEUE, sequence = 99999.0d, reload = true, version = "2")
 @Updater(name = "Test Updater 3", path = "", xpath = "//element(*, hippo:document)"
-        , description = "Test thing", batchSize = 1L, throttle = 200L, dryRun = true, parameters = "{prop: val}")
+        , description = "Test things, like reserved words as import or others", batchSize = 1L, throttle = 200L, dryRun = true, parameters = "{prop: val}")
 
 class TestUpdater3 extends BaseNodeUpdateVisitor {
     boolean doUpdate(Node node) {
